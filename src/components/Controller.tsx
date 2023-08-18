839485e1a68abb59f35fb749592a0a0d6eef3544
Marcus Bello <marcusthetechguru@gmail.com>
2023-08-18 01:11:53 -0700

    first commit

---
+++ src/components/Controller.tsx
@@ -0,0 +1,11 @@
+
+
+function Controller() {
+  return (
+    <div className="container bg-green-300 mx-auto">
+      <button className="transition-all duration-300 border border-black w-full mt-4 px-4 py-2 rounded-sm bg-indigo-500 hover:bg-indigo-600">Place Bet</button>
+    </div>
+  )
+}
+
+export default Controller
